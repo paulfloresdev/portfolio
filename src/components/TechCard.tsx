8c5040841aa526fbeead53cac5b42cc191456ba5
Paul Flores <165419669+paulfloresdev@users.noreply.github.com>
2026-01-08 20:30:34 -0700

    updates at techs

--- src/components/TechCard.tsx
+++ src/components/TechCard.tsx
@@ -7,8 +7,8 @@ interface TechCardProps {
 
 const TechCard: React.FC<TechCardProps> = ({ label, image }) => {
     return <div className="w-tech-mobile md:w-tech-desktop flex flex-col items-center justify-center space-y-4">
-        <img src={image} alt={label} className="w-full h-tech-mobile md:h-tech-desktop"/>
-        <h1 className="text-sm font-light">{label}</h1>
+        <img src={image} alt={label} className="w-full h-tech-mobile md:h-tech-desktop" />
+        <h1 className="text-sm font-light text-center">{label}</h1>
     </div>
 }
 
--- src/components/TechGrid.tsx
+++ src/components/TechGrid.tsx
@@ -2,18 +2,18 @@ import React, { useEffect, useState } from "react";
 import TechCard from "./TechCard";
 
 const TechGrid: React.FC = () => {
-    const [techs,setTechs] = useState([]);
+    const [techs, setTechs] = useState([]);
 
     useEffect(() => {
         fetch("/data.json")
-          .then((response) => response.json())
-          .then((data) => setTechs(data.techs))
-          .catch((error) => console.error("Error al cargar los datos:", error));
+            .then((response) => response.json())
+            .then((data) => setTechs(data.techs))
+            .catch((error) => console.error("Error al cargar los datos:", error));
     }, []);
 
     return <div className="grid grid-cols-3 md:grid-cols-6 gap-y-8 md:gap-y-12 gap-x-16 mt-8 md:mt-12 place-items-center items-start">
         {techs.map((tech, index) => (
-            <TechCard key={index} label={tech['label']} image={tech['image']}/>
+            <TechCard key={index} label={tech['label']} image={tech['image']} />
         ))}
     </div>
 }
